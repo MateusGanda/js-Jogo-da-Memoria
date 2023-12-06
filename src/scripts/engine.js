d55ddd3f4e7e22f8b525008646a672e78bf0ef42
Mateus Ganda <151588488+MateusGanda@users.noreply.github.com>
2023-12-05 23:05:49 -0300

    Update engine.js

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,58 +1,59 @@
-const emojis = [
-  "🐱",
-  "🐱",
-  "🦝",
-  "🦝",
-  "🦊",
-  "🦊",
-  "🐶",
-  "🐶",
-  "🐵",
-  "🐵",
-  "🦁",
-  "🦁",
-  "🐯",
-  "🐯",
-  "🐮",
-  "🐮",
-];
-let openCards = [];
+const emojis = [//Todos os emojis
+    "💵",
+    "💵",
+    "😱",
+    "😱",
+    "😡",
+    "😡",
+    "🤡",
+    "🤡",
+    "🦁",
+    "🦁",
+    "🎂",
+    "🎂",
+    "🐘",
+    "🐘",
+    "😨",
+    "😨"
+]; 
+let openCards = []; //Vai guardar ar cartas que forem abertas
 
-let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-for (let i = 0; i < emojis.length; i++) {
-  let box = document.createElement("div");
-  box.className = "item";
-  box.innerHTML = shuffleEmojis[i];
-  box.onclick = handleClick;
-  document.querySelector(".game").appendChild(box);
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2: -1)); 
+//Se for maior que 0.5 vai dar 2, senão vai dar -1
+
+for(let i = 0; i < emojis.length; i++){ //Vai criar um elemento com a tag div
+    let box = document.createElement("div");
+    box.className = "item"; //vai ter uma classe com nome item
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").appendChild(box); //Vai adicionar no site
 }
 
 function handleClick() {
-  if (openCards.length < 2) {
-    this.classList.add("boxOpen");
-    openCards.push(this);
-  }
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen"); //adiciona a class boxOpen quando clicar na carta
+        openCards.push(this);
+    }
 
-  if (openCards.length == 2) {
-    setTimeout(checkMatch, 500);
-  }
-
-  console.log(openCards);
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
 }
 
 function checkMatch() {
-  if (openCards[0].innerHTML === openCards[1].innerHTML) {
-    openCards[0].classList.add("boxMatch");
-    openCards[1].classList.add("boxMatch");
-  } else {
-    openCards[0].classList.remove("boxOpen");
-    openCards[1].classList.remove("boxOpen");
-  }
+    if(openCards[0].innerHTML === openCards[1].innerHTML) {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");// Adiciona o boxMatch
+    }else{
+        openCards[0].classList.remove("boxOpen"); //se n baterem certo, eles se fecham
+        openCards[1].classList.remove("boxOpen");
+    }
 
-  openCards = [];
+    openCards = []; //Retorna ao vetor vazio
 
-  if (document.querySelectorAll(".boxMatch").length === emojis.length) {
-    alert("Você venceu !");
-  }
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){ //Se a quantidade de boxMatch for igual a emojis.lenght, envia um alerta
+        alert("Você venceu!");
+    }
+}
 }
